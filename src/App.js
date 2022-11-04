a1ee4d851eb878a9354ac97c51e3c24335a1540c
roxibi <106161599+roxibi@users.noreply.github.com>
2022-11-04 11:31:09 +0100

    add country

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [ip, setIp] = useState();
+  const [country, setCountry]=useState();
   const getIP = () => {
     const url =
       "https://geolocation-db.com/json/";
@@ -10,6 +11,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         setIp(data.IPv4);
+        setCountry(data.country_name)
       })
       
   };
@@ -25,7 +27,7 @@ console.log(ip)
 //   useEffect(()=>{
 //     getLocation()
 //   }, []);
-  return <><div className="App">This is your IP: {ip}</div>
+  return <><div className="App">This is your IP: {ip} <br/> I know where you are in {country}</div>
   {/* <p>This is your location: {location}</p> */}
   </>;
 }
